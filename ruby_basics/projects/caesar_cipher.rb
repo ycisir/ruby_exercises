c39f60a0b3a11cecea9ab9033a9aa55a59e72998
= <=>
2026-08-20 06:13:27 +0530

    implement caesar cipher

--- ruby_basics/projects/caesar_cipher.rb
+++ ruby_basics/projects/caesar_cipher.rb
@@ -1,6 +1,14 @@
-def caesar_cipher(message, key)
-	message.split('')
+def caesar_cipher(message, key=1)
+	encrypt_message = message.chars.map do |c|
+		case c
+		when 'a'..'z' then ((((c.ord - 97 + key) % 26) + 97).chr)
+		when 'A'..'Z' then ((((c.ord - 65 + key) % 26) + 65).chr)
+		else
+			c
+		end
+	end
+	encrypt_message.join
 end
 
 
-p caesar_cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 5)
